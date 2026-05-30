6ff2e934c886a4938c46a3e399f75536b8b2f35b
NathanielAdams2000 <109816911+NathanielAdams2000@users.noreply.github.com>
2026-05-30 16:06:43 +0100

    Delete shs_portal directory

--- shs_portal/new.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo password_hash("12345", PASSWORD_DEFAULT);
-?>
-
-
-$2y$10$wUwSFD3DY3wrVXTb3mPBT.34hJdlbAw7w8FKVvDXF7OCq8yO5Erhy
--- shs_portal/students/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('../db/connect.php');
-$id = $_GET['student_id'];
-pg_query($conn, "DELETE FROM students WHERE id=$id");
-header("Location: index.php");
-exit();
-?>
