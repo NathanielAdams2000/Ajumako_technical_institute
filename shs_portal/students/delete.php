8940462c9f0a41e2dfcdba0b2eca9f144d26577e
NathanielAdams2000 <109816911+NathanielAdams2000@users.noreply.github.com>
2026-06-07 15:53:41 +0100

    delete.php

--- shs_portal/students/delete.php
+++ shs_portal/students/delete.php
@@ -1,7 +1,17 @@
 <?php
 include('../db/connect.php');
-$id = $_GET['student_id'];
-pg_query($conn, "DELETE FROM students WHERE id=$id");
+
+if (isset($_GET['student_id'])) {
+
+    $id = $_GET['student_id'];
+
+    pg_query_params(
+        $conn,
+        "DELETE FROM students WHERE student_id = $1",
+        [$id]
+    );
+}
+
 header("Location: index.php");
 exit();
 ?>
