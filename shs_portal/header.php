eb27fad3c8c5bd9ffe4eaa5fb5efd0fdf649bf83
NathanielAdams2000 <109816911+NathanielAdams2000@users.noreply.github.com>
2026-06-07 12:32:27 +0100

    header.php

--- shs_portal/header.php
+++ shs_portal/header.php
@@ -2,10 +2,7 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-if (!isset($_SESSION['user'])) {
-    header("Location: ../login.php");
-    exit();
-}
+
 $username = $_SESSION['user'];
 
 $currentPage = basename($_SERVER['PHP_SELF']);
