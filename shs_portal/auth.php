da1592b47d087762f77505a577a936e0076756b8
NathanielAdams2000 <109816911+NathanielAdams2000@users.noreply.github.com>
2026-06-07 18:05:21 +0100

    auth.php

--- shs_portal/auth.php
+++ shs_portal/auth.php
@@ -1,7 +1,8 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
-// ❌ Not logged in
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit();
