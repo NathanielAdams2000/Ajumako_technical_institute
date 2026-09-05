65a9adf7ed3d36a9827a1d13538740873354d80a
NathanielAdams2000 <109816911+NathanielAdams2000@users.noreply.github.com>
2026-09-05 02:44:29 +0000

    Delete shs_portal directory

--- shs_portal/courses/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('../db/connect.php');
-$id = $_GET['id'];
-pg_query($conn, "DELETE FROM students WHERE id=$id");
-header("Location: index.php");
-exit();
-?>
--- shs_portal/exams_score/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('../db/connect.php');
-$id = $_GET['id'];
-pg_query($conn, "DELETE FROM teachers WHERE id=$id");
-header("Location: index.php");
-exit();
-?>
